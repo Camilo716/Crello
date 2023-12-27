3afbdd1a0b118af313ac73a4d9fab703debb2446
camilogs <camilo.gonzalez.col@gmail.com>
2023-12-27 12:02:48 -0500

    Move routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\CardListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/card', [CardController::class,'store']);
+Route::put('/card/{id}', [CardController::class,'update']);
+Route::delete('/card/{id}', [CardController::class, 'destroy']);
+Route::get('/card/get-by-list', [CardController::class,'getByList']);
+Route::patch('/card/patch-parent-list/{id}', [CardController::class, 'patchParentList']);
+
+Route::get('/card-list', [CardListController::class,'index']);
+Route::post('/card-list', [CardListController::class,'store']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\CardController;
-use App\Http\Controllers\CardListController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('board');
 });
-
-Route::post('/api/card', [CardController::class,'store']);
-Route::put('/api/card/{id}', [CardController::class,'update']);
-Route::delete('/api/card/{id}', [CardController::class, 'destroy']);
-Route::get('/api/card/get-by-list', [CardController::class,'getByList']);
-Route::patch('/api/card/patch-parent-list/{id}', [CardController::class, 'patchParentList']);
-
-Route::get('/api/card-list', [CardListController::class,'index']);
-Route::post('/api/card-list', [CardListController::class,'store']);
