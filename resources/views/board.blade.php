0f1019778338efab1610f5815a0bfb19d150318f
camilogs <camilo.gonzalez.col@gmail.com>
2024-01-07 17:00:04 -0500

    Add new board form

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,23 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Board</title>
     <link rel="stylesheet" href="{{ asset('css/board.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        crossorigin="anonymous">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;1,500;1,900&family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;1,500;1,900&family=Roboto+Condensed:wght@300;400&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;1,500;1,900&family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;1,500;1,900&family=Roboto+Condensed&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
     <nav>
-        <div class='boardsContainer' id='boardsContainer'>
-            <!-- Boards will be dynamically added here -->
+        <div class="navBoards">
+            <div class='boardsContainer' id='boardsContainer'>
+                <!-- Boards will be dynamically added here -->
+
+            </div>
+            <div class=''>
+                <form action="POST" class="newBoardForm">
+                    @csrf
+                    <input type="text" required placeholder="Enter board title..." id="board-title"
+                        class="formInput"></input>
+                    <button id="addNewBoardButton" class="addNewList" type="button">Add Board</button>
+                </form>
+            </div>
         </div>
     </nav>
     <div class='container'>
@@ -27,7 +45,8 @@
         <div class='newListContainer'>
             <form action="POST" class="newListForm">
                 @csrf
-                <input type="text" required placeholder="Enter list title..." id="title"></input>
+                <input type="text" required placeholder="Enter list title..." id="title"
+                    class="formInput"></input>
                 <button id="addNewList" class="addNewList" type="button">Add list</button>
             </form>
         </div>
@@ -35,4 +54,5 @@
 
     <script src='{{ asset('js/main.js') }}'></script>
 </body>
+
 </html>
